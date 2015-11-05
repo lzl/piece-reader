48c2876d7593d8e9026d72444ef3145aa00350ef
lizunlong <lizunlong@gmail.com>
2015-11-05 22:06:42 +0800

    updated method logic

--- both/methods.js
+++ both/methods.js
@@ -32,9 +32,14 @@ Meteor.methods({
 
     var exitedHostname = !! Subs.findOne({ownerId: ownerId, hostname: hostname});
     if (exitedHostname) {
-      return Subs.update({ownerId: ownerId, hostname: hostname}, {
-        $pull: {userId: userId}
-      });
+      var onlyOne = Subs.findOne({ownerId: ownerId, hostname: hostname}).userId.length;
+      if (onlyOne === 1) {
+        return Subs.remove({ownerId: ownerId, hostname: hostname});
+      } else {
+        return Subs.update({ownerId: ownerId, hostname: hostname}, {
+          $pull: {userId: userId}
+        });
+      }
     }
   }
 });
