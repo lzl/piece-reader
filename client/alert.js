f1151abb0b7030a5f7dd8f8cc8450d0141c07a4b
lizunlong <lizunlong@gmail.com>
2015-12-21 12:07:33 +0800

    show alert when there is new piece
    learn from http://blog.percolatestudio.com/design/design-for-realtime/

---
+++ client/alert.js
@@ -0,0 +1,42 @@
+Template.alertNewPieceWrapper.onCreated(function () {
+  const instance = this;
+  instance.state = new ReactiveDict();
+  instance.state.setDefault({
+    count: 0
+  })
+
+  instance.autorun(() => {
+    const timestamp = Session.get('lastestRefreshTimestamp');
+    const count = LocalPieces.find({createdAt: {$gt: timestamp}}).count();
+    instance.state.set('count', count);
+  })
+})
+Template.alertNewPieceWrapper.helpers({
+  hasPiece() {
+    const timestamp = Session.get('lastestRefreshTimestamp');
+    return LocalPieces.findOne({createdAt: {$gt: timestamp}});
+  },
+  count() {
+    const instance = Template.instance();
+    return instance.state.get('count');
+  }
+})
+
+Template.alertNewPiece.helpers({
+  text() {
+    const instance = Template.instance();
+    const count = instance.data.count;
+    if (count > 1) {
+      return `${count} new pieces`;
+    } else {
+      return "1 new piece";
+    }
+  }
+})
+Template.alertNewPiece.events({
+  'click [data-action=refresh]': (event, instance) => {
+    event.preventDefault();
+    Session.set('lastestRefreshTimestamp', new Date());
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  }
+})
